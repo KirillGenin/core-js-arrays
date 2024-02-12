d61e3e27a108bca6b396e85300544b3fac43a4b2
KirillGenin <332211op@gmail.com>
2024-02-12 18:10:33 +0300

    feat: implement function findCommonElements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -500,8 +500,8 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return Array.from(new Set(arr1.filter((item) => arr2.includes(item))));
 }
 
 /**
